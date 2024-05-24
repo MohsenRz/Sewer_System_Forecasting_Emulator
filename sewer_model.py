50744df67b388610141133a591801558f5d946af
MohsenRz <mrezaee1248@gmail.com>
2024-05-24 18:09:27 +0100

    new version of the code with new kernels

--- sewer_model.py
+++ sewer_model.py
@@ -3,12 +3,13 @@ Simulation code based on PySWMM engine
 getting data for a GP prediction
 setting up the kernel 
 Author: Mohsen Rezaee
-Version: 2
-Date: 16/04/2024
+Version: 3
+Date: 24/04/2024
 '''
 import time
 import datetime
 start_t = time.time()
+import pickle
 from pyswmm import Simulation, Nodes, Links
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.preprocessing import StandardScaler
@@ -16,10 +17,13 @@ from sklearn.gaussian_process.kernels import RBF, ExpSineSquared, RationalQuadra
 import numpy as np
 import matplotlib.pyplot as plt 
 
+start_t_p1 = time.time()
+"""
+#first part
 with Simulation(r'model_2_1.inp') as sim:
     # defining start and end times instead of getting it from the simulator
     sim.start_time = datetime.datetime(2023, 1, 1, 0, 0, 0) # (year, month, day, hour, minute, second)
-    sim.end_time = datetime.datetime(2023, 5, 1, 0, 0, 0)
+    sim.end_time = datetime.datetime(2023, 7, 1, 0, 0, 0)
     
     print("Simulation info:")
     flow_units = sim.flow_units
@@ -49,12 +53,29 @@ with Simulation(r'model_2_1.inp') as sim:
           CSO_discharge.append(CSO.total_inflow)
           
           simtime.append(sim.current_time)
-
-GP_dict_test = {t: (influent, overflows) for t, influent, overflows in zip((range(1201, 1229)), WWTP_inflow[201:229], CSO_discharge[201:229])}
-
-GP_dict1 = {t: (influent, overflows) for t, influent, overflows in zip(range(1, 1001), WWTP_inflow[1:1001], CSO_discharge[1:1001])}  
-GP_dict2 = {t: (influent, overflows) for t, influent, overflows in zip(range(1120, 1200), WWTP_inflow[1120:1200], CSO_discharge[1120:1200])}  
+# saving the produced data 
+data_from_pyswmm = {'WWTP_inflow': WWTP_inflow, 
+                    'CSO_discharge': CSO_discharge,
+                    'simtime': simtime}
+with open('sim_data_2013to2025_1hr_intervals.pkl', 'wb') as f:
+  pickle.dump(data_from_pyswmm, f)
+"""
+#Loading data
+with open('sim_data_2013to2025_1hr_intervals.pkl', 'rb') as f: 
+  loaded_data = pickle.load(f)
+WWTP_inflow = loaded_data['WWTP_inflow']
+CSO_discharge = loaded_data['CSO_discharge']
+end_t_p1 = time.time()
+print(f'The loading time was {round(end_t_p1 - start_t_p1)} seconds')
+# --------------------------------------------------
+start_t_p2 = time.time()
+strt1, strt2 = 1, 2001
+end1, end2 = 2000, 3000
+GP_dict1 = {t: (influent, overflows) for t, influent, overflows in zip(range(strt1, end1), WWTP_inflow[strt1:end1], CSO_discharge[strt1:end1])}  
+GP_dict2 = {t: (influent, overflows) for t, influent, overflows in zip(range(strt2, end2), WWTP_inflow[strt2:end2], CSO_discharge[strt2:end2])}  
 GP_dict = {**GP_dict1, **GP_dict2}
+
+GP_dict_test = {t: (influent, overflows) for t, influent, overflows in zip((range(3001, 4000)), WWTP_inflow[3001:4000], CSO_discharge[3001:4000])}
 
 hours = np.array([hr for hr, _ in GP_dict.items()]).reshape(-1, 1)
 WWTP_influent = np.array([inf for inf, _ in GP_dict.values()]).reshape(-1, 1)
@@ -72,14 +93,12 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 # Define the kernel
-longterm_kernel = RBF(length_scale=1.0e-2, length_scale_bounds=(1e-7, 1e7))
-daily_kernel = 10.0 * ExpSineSquared(length_scale=0.01, periodicity=24.0, periodicity_bounds="fixed")
-seasonal_kernel = ExpSineSquared(length_scale=0.01, periodicity=24*30*1) # here I get monthly changes 
-#irregularities_kernel = 0.5**2 * RationalQuadratic(length_scale=1.0, alpha=1.0)
-lnkernel = 1.0 * DotProduct(1)
-kernel = daily_kernel 
+daily_kernel = ExpSineSquared (length_scale=0.01, periodicity=3, length_scale_bounds=(5e-3, 1), periodicity_bounds="fixed")
+Longterm_variations = ExpSineSquared (length_scale=0.002, periodicity=72, length_scale_bounds=(1e-3, 1e-1), periodicity_bounds="fixed")
+
+kernel = daily_kernel + 10 * Longterm_variations
 # Instantiate the Gaussian Process Regression model
-GPR = GaussianProcessRegressor(kernel=longterm_kernel, alpha= 1e-2, normalize_y=True)
+GPR = GaussianProcessRegressor(kernel=kernel, alpha=0.1 , normalize_y=True)
 GPR.fit(X_train_scaled, y_train)
 
 kernel_params = GPR.kernel_.get_params()  # getting kernel parameters 
@@ -92,6 +111,9 @@ sorted_indices = np.argsort(X_test.ravel())
 X_test_sorted = X_test[sorted_indices]
 y_test_sorted = y_test[sorted_indices]
 WWTP_inflow_predict_sorted = WWTP_influent_predict[sorted_indices] 
+
+end_t_p2 = time.time()
+print(f'The running time for our GPR was {round(end_t_p2 - start_t_p2)} seconds')
 
 # Plot the predictions
 plt.figure(figsize=(12, 6))
@@ -114,6 +136,6 @@ plt.title('Gaussian Process Regression: WWTP Inflow Prediction')
 plt.legend()
 
 end_t = time.time()
-print(f'running time was {round(end_t - start_t, 2)} seconds')
+print(f'Total running time was {round(end_t - start_t, 2)} seconds')
 
 plt.show()
